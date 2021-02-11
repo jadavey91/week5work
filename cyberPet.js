36d7a7a998f2e6334c9f38309ec538636c8b37b6
Jim <jadavey@hotmail.com>
2021-02-11 10:14:17 +0000

    Something I did last night

--- cyberPet.js
+++ cyberPet.js
@@ -1,37 +1,45 @@
-class Pet{
-    constructor(type){
-        this.type = type;
-        this.hunger = 5;
-        this.thirst = 5;
-        this.fun = 5;
+class Pet {
+  constructor(type) {
+    this.type = type;
+    this.hunger = 5;
+    this.thirst = 5;
+    this.fun = 5;
+  }
+  play() {
+    this.fun++, this.hunger--, this.thirst--;
+    console.log(
+      `That was fun! Here's how I'm doing - \n hunger:${this.hunger} \n thirst:${this.thirst} \n fun:${this.fun}`
+    );
+    return this.petUpdate();
+  }
+  feed() {
+    this.fun--, this.hunger++, this.thirst--;
+    console.log(
+      `That was tasty! Here's how I'm doing - \n hunger:${this.hunger} \n thirst:${this.thirst} \n fun:${this.fun}`
+    );
+    return this.petUpdate();
+  }
+  drink() {
+    this.fun--, this.hunger--, this.thirst++;
+    console.log(
+      `That was tasty! Here's how I'm doing - \n hunger:${this.hunger} \n thirst:${this.thirst} \n fun:${this.fun}`
+    );
+    return this.petUpdate();
+  }
+  petUpdate() {
+    if (this.fun <= 2) {
+      console.log(`You need to play with me, I'm only having ${this.fun} with you!`
+      );
     }
-    play() {
-        this.fun++, this.hunger--, this.thirst--;
-        console.log(`That was fun! Here's how I'm doing - \n hunger:${this.hunger} \n thirst:${this.thirst} \n fun:${this.fun}`);
-        return this.petUpdate();
+    if (this.thirst <= 2) {
+      console.log(`You need to hydrate me, I'm ${this.thirst} and thirsty!`);
     }
-    feed() {
-        this.fun--, this.hunger++, this.thirst--;
-        console.log(`That was tasty! Here's how I'm doing - \n hunger:${this.hunger} \n thirst:${this.thirst} \n fun:${this.fun}`);
-        return this.petUpdate();
+    if (this.hunger <= 2) {
+      console.log(`You need to feed me, I am ${this.hunger} and starving!`);
+      // } if (this.hunger > 2 && this.thirst > 2 && this.fun > 2) { // had to add this as the else statement runs even when an above if is met?
+      //     console.log(`I'm happy to do something. Would you like to play, feed or hydrate me?`);
     }
-    drink() {
-        this.fun--, this.hunger--, this.thirst++;
-        console.log(`That was tasty! Here's how I'm doing - \n hunger:${this.hunger} \n thirst:${this.thirst} \n fun:${this.fun}`);
-        return this.petUpdate();
-    }
-    petUpdate() {
-        if (this.fun <= 2) {
-            console.log(`You need to play with me, I'm only having ${this.fun} with you!`)
-        } if (this.thirst <=2) {
-            console.log(`You need to hydrate me, I'm ${this.thirst} and thirsty!`)
-        } if (this.hunger <= 2) {
-            console.log(`You need to feed me, I am ${this.hunger} and starving!`)
-        } if (this.hunger > 2 && this.thirst > 2 && this.fun > 2) { // had to add this as the else statement runs even when an above if is met?
-            console.log(`I'm happy to do something. Would you like to play, feed or hydrate me?`);
-        } else { //not sure how to have an else to end these if statements?
-        }
-}
+  }
 }
 
 // const petUpdate = (animal) => {
@@ -46,7 +54,6 @@ class Pet{
 //         console.log(`I'm happy to do something. Would you like to play, feed or hydrate me?`);
 //     }
 // }
-
 
 const bill = new Pet("Dog");
 // console.log(Bill);
